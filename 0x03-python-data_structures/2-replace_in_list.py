19f12583db15e94d46fe024a24fa97506472f25d
AngieKoki <angie.dar66@gmail.com>
2022-09-26 16:39:12 +0000

    Correct pycodstyle error

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-#Replacing elements of a list at a specific index
-
 def replace_in_list(my_list, idx, element):
     len_list = len(my_list)
     if (idx < 0 or idx > len_list - 1):

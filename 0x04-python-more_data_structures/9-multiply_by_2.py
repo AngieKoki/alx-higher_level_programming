4ff11cea0037b609ec5d698f52acfca137eb1306
AngieKoki <angie.dar66@gmail.com>
2022-09-28 16:13:17 +0000

    Hot fix code

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,7 +3,7 @@
 # Returns a new dictionary with all values multiplies by 2
 
 def multiply_by_2(a_dictionary):
-    new_dict = {key: value for key, value in a_dictionary}
+    new_dict = {key: value for key, value in a_dictionary.items}
     for i in new_dict:
         new_dict[i] *= 2
     return (new_dict)

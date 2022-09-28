e73d4a82791cf372d26af2343603e17414c0189e
AngieKoki <angie.dar66@gmail.com>
2022-09-28 12:43:52 +0000

    Hot fix syntax error

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,7 @@
 # computes the square value of all integers of a matrix
 
 def square_matrix_simple(matrix=[]):
-    res=[]
+    res = []
     for item in matrix:
         res.append(list(map(lambda x: x**2, item)))
     return(res)

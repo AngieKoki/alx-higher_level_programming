410445f224c8204930deaae91305efd989c1f35e
AngieKoki <angie.dar66@gmail.com>
2022-09-28 12:37:32 +0000

    Squared simple

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+#a function that computes the square values of all integers of a matrix
+
+def square_matrix_simple(matrix=[]):
+    res[]
+    for item in matrix:
+        res.append(list(map(lambda x: x**2, item)))
+    return(res)

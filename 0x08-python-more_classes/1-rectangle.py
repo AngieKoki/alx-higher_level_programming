1384af011787338201e09c6b01843f073f783e0b
AngieKoki <angie.dar66@gmail.com>
2022-10-19 14:40:16 +0000

    Fixing the psycodestyle agaaain!

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""Defines a rectangle"""
-
+"""This function defines a rectangle"""
 
 
 class Rectangle:
     """The __init__ method initialize the values passed to the object.
     Attributes:
-        width: private attribute
-        height: private attribute
+        width: is a private attribute.
+        height: is a private attribute.
     """
-
 
     def __init__(self, width=0, height=0):
         self.width = width

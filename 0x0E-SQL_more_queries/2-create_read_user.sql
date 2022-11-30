710e06d50d8b0a606c6c749cd3d6832630cf51b9
AngieKoki <angie.dar66@gmail.com>
2022-11-30 05:18:32 -0800

    Creating a databse and a user and granting privileges

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,16 @@
+-- Creates a database and a user
+-- User should have only SELECT privilefe in the database
+-- User password should be set to user_0d_2_pwd
+-- If the database hbtn_0d_2 already exists, your script should not fail
+-- If the user user_0d_2 already exists, your script should not fail
+
+CREATE DATABASE 
+	IF NOT EXISTS `hbtn_0d_2`;
+
+CREATE USER
+	IF NOT EXISTS 'user_0d_1'@'localhost'
+	IDENTIFIED BY 'user_0d_2_pwd';
+
+GRANT SELECT
+	ON `hbtn_0d_2`.*
+	TO 'user_0d_2'@'localhost';
